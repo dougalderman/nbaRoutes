34e817e8d2ac44e828cd5303b178c801b978d46f
Doug Alderman <agnadcp-d1@yahoo.com>
2016-01-04 17:31:54 -0700

    1/4/16 changes

--- js/app.js
+++ js/app.js
@@ -6,4 +6,29 @@ app.config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
 
     // routing configuration code
 
+    $urlRouterProvider
+      .otherwise('/');
+
+      $stateProvider
+  	    .state('home', {
+  			    templateUrl: 'js/home/homeTmpl.html',
+  			    url: '/',
+            controller: 'homeCtrl'
+  		  })
+        .state('teams', {
+  			    templateUrl: 'js/teams/teamTmpl.html',
+  			    url: '/teams/:team',
+            controller: 'teamCtrl',
+            resolve: {
+              teamData: function(teamService, $stateParams) {
+                return teamService.getTeamData($stateParams.team);
+              }
+            }
+  		  })
+        .state('products', {
+            templateUrl: 'js/products/productTmpl.html',
+            url: '/products/:id',
+            controller: 'productsCtrl'
+        });
+
 });
--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -3,5 +3,57 @@ var app = angular.module('nbaRoutes');
 app.service('teamService', function ($http, $q) {
 
     // service code
+    this.addNewGame = function(gameObj) {
+
+        var url = 'https://api.parse.com/1/classes/' + gameObj.homeTeam;
+        if (parseInt(gameObj.homeTeamScore) > parseInt(gameObj.opponentScore))
+          gameObj.won = true;
+        else {
+          gameObj.won = false;
+        }
+        return $http({
+          method: 'POST',
+          url: url,
+          data: gameObj
+        })
+    };
+
+    this.getTeamData = function(team) {
+      var deferred = $q.defer();
+      var url = 'https://api.parse.com/1/classes/' + team;
+      $http({
+        method: 'GET',
+        url: url
+      })
+      .then(function(data) {
+        if (data.status === 200) {
+           var results = data.data.results;
+           var wins = 0;
+           var losses = 0;
+           for (var i = 0; i < results.length; i++) {
+             if (results[i].won)
+                wins++;
+             else {
+                losses++;
+             }
+           }
+           results.wins = wins;
+           results.losses = losses;
+           deferred.resolve(results);
+        }
+        else {
+          console.log('status code is ' + data.status)
+          deferred.reject(data.status);
+        }
+      })
+      .catch(function(err) {
+        deferred.reject(err);
+      })
+
+    return deferred.promise;
+
+    };
+
+
 
 });
